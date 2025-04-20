4632767819c13a05d961926899350103cfd52fe1
Alper Erten <55757891+alpererten@users.noreply.github.com>
2025-04-20 16:16:38 -0700

    deleted folder

--- ros2_scene_graph_generator_v3/build/custom_msgs/rosidl_generator_py/custom_msgs/msg/__init__.py
+++
@@ -1,6 +0,0 @@
-from custom_msgs.msg._detected_surface import DetectedSurface  # noqa: F401
-from custom_msgs.msg._detected_surfaces import DetectedSurfaces  # noqa: F401
-from custom_msgs.msg._inference_result import InferenceResult  # noqa: F401
-from custom_msgs.msg._segmentation_result import SegmentationResult  # noqa: F401
-from custom_msgs.msg._yolo_inference import YoloInference  # noqa: F401
-from custom_msgs.msg._yolo_segmentation_msg import YoloSegmentationMsg  # noqa: F401
